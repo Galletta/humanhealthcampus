0b9bf554128f3b140b78b3b382876dc9ba6853b6
Sergey Subbotin <sergey@devmiles.com>
2013-08-27 07:56:19 +0300

    Updated styles

--- sites/all/themes/tweme/assets/js/drupal.js
+++ sites/all/themes/tweme/assets/js/drupal.js
@@ -2,5 +2,10 @@
   $(document).ready(function() {
     $('.field-type-text-long table').addClass('table table-bordered');
     $('.field-type-text-with-summary table').addClass('table table-bordered');
+
+	$(".library-link").click( function() { 
+	     $(this).parent().toggleClass('active');
+	     return false;
+	});
   });
 })(jQuery);
